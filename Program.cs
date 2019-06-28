6f444777a13563fd2fd08a78dbdeef7fc6404061
Toby Samples <tsamples@psclistens.com>
2019-06-28 13:34:03 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TempChangeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a valid Fahrenheit temperature.");
+            string strDegText = Console.ReadLine();
+            double DegText, CelText;
+            DegText = double.Parse(strDegText);
+            CelText = (5.0 / 9.0) * (DegText - 32.0);
+            Console.WriteLine("It is " + CelText.ToString() + " degrees in Celsius.");
+
+            if (DegText > 100)
+            {
+                Console.WriteLine("It's Hot! Better Hydrate");
+
+            }
+            else if (DegText <= 32)
+            {
+                Console.WriteLine("It's Cold! Better pack long underwear");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
